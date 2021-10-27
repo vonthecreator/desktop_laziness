9c35c0996209ef1e36ac7d714bc56dd6b72c803a
munazhe <munazhe@xoza.phub.net.cable.rogers.com>
2021-10-26 20:04:56 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# desktop_laziness 
+
+NOTE: This entire project was designed for mac use only. 
+
+## 0x0001 - What's in the package: 	
+	1. `watch_desktop.py` --> Keeps your desktop clean in one folder (desktop_laziness)
+	
+
+### 0x0002 -- How to setup: 
+	1. Install dependecies: `pip install -r requirements.txt`
+	2. Move all `desktop_laziness` files into `~/.sourcecode/desktop_laziness/`
+
+
+### 0x0003 - Running your programs (Mac only!)
+	1. `watch_desktop.py`: `python watch_desktop.py <username> <collection_name>` (Where `collection_name` == New desktop folder name)
+
+
+> You can also add `&` at the end of the command so that it runs as a services in the background.
---
+++ watch_desktop.py
@@ -0,0 +1,55 @@
+
+# HOW TO USE & HOW IT WORKS
+# When the script is initiated it listens for changes 
+# If any file is modified or touch then the watchdog program will 
+# run the appropriate sub-routines to mirror both folders
+# move_files.py SRC DEST
+
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from watchdog.events import LoggingEventHandler
+
+import os 
+import logging
+import json
+import time
+import sys
+
+
+class MyHandler(FileSystemEventHandler): 
+	def on_modified(self, event):
+		for filename in os.listdir(src_folder):
+			src = src_folder + "/" + filename
+			dest = dest_folder + "/" + filename
+			if filename != collection_name:
+				os.rename(src, dest) #move file(s)
+
+
+
+username = sys.argv[1] #parse param1 (source folder) from CLI
+collection_name = sys.argv[2] #parse param2 (destination folder) from CLI
+src_folder = "/Users/"+username+"/Desktop"
+dest_folder = "/Users/"+username+"/Desktop/"+collection_name
+event_handler = MyHandler()
+event_logger = LoggingEventHandler()
+
+## log changes
+logging.basicConfig(level=logging.INFO,
+                      format='%(asctime)s - %(message)s',
+                      datefmt='%Y-%m-%d %H:%M:%S')
+
+observer = Observer()
+observer.schedule(event_handler, src_folder, recursive = True)
+observer.start() #start monitoring changes
+
+
+
+try: 
+	while True:
+		time.sleep(10)
+except KeyboardInterrupt: #unless I hit ctr-c
+
+
+	observer.stop()
+	observer.join()
